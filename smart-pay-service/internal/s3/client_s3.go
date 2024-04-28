80fb6276837449231b0db9b19e76b55b6e16ec25
patrik-rangel <patrik.souza@sptech.school>
2024-04-28 15:20:06 -0300

    Download CSV

--- smart-pay-service/internal/s3/client_s3.go
+++ smart-pay-service/internal/s3/client_s3.go
@@ -3,17 +3,36 @@ package s3_client
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	log_zap "smart-pay-service/config/log"
+	"strings"
+	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/joho/godotenv"
+	"github.com/spf13/afero"
 	"go.uber.org/zap"
 )
+
+var mesesMap = map[string]string{
+	"january":   "janeiro",
+	"february":  "fevereiro",
+	"march":     "março",
+	"april":     "abril",
+	"may":       "maio",
+	"june":      "junho",
+	"july":      "julho",
+	"august":    "agosto",
+	"september": "setembro",
+	"october":   "outubro",
+	"november":  "novembro",
+	"december":  "dezembro",
+}
 
 type S3Handler struct {
 	region string
@@ -71,11 +90,49 @@ func (h *S3Handler) ListObjects() []string {
 
 	response := make([]string, 0, len(output.Contents))
 	for _, object := range output.Contents {
-		h.log.Info(fmt.Sprintf("key=%s size=%d", aws.ToString(object.Key), object.Size))
 		response = append(response, aws.ToString(object.Key))
 	}
 
+	response = currentMonthCsv(response)
+	h.log.Info(fmt.Sprintf("response List Object %v", response))
 	return response
+}
+
+func (h *S3Handler) DownloadCsv(bucketName string, objectKey string, fileName string) error {
+	var aferoFs = afero.NewOsFs()
+	result, err := h.client.GetObject(context.TODO(), &s3.GetObjectInput{
+		Bucket: aws.String(bucketName),
+		Key:    aws.String(objectKey),
+	})
+	if err != nil {
+		log.Printf("Couldn't get object %v:%v. Here's why: %v\n", bucketName, objectKey, err)
+		return err
+	}
+	defer result.Body.Close()
+
+	nameDir := strings.Split(objectKey, "/")[0]
+	existsDir, _ := afero.Exists(aferoFs, fmt.Sprintf("tools/%s", nameDir))
+
+	if !existsDir {
+		err = aferoFs.Mkdir(fmt.Sprintf("tools/%s", nameDir), 0755)
+		if err != nil {
+			log.Printf("Couldn't create dir %s error: %s", nameDir, err)
+			return err
+		}
+	}
+
+	file, err := aferoFs.Create(fmt.Sprintf("tools/%s/%s", nameDir, fileName))
+	if err != nil {
+		log.Printf("Couldn't create file %v. Here's why: %v\n", fileName, err)
+		return err
+	}
+	defer file.Close()
+	body, err := io.ReadAll(result.Body)
+	if err != nil {
+		log.Printf("Couldn't read object body from %v. Here's why: %v\n", objectKey, err)
+	}
+	_, err = file.Write(body)
+	return err
 }
 
 func getKeys() (*keys, error) {
@@ -89,4 +146,27 @@ func getKeys() (*keys, error) {
 		keyId:     os.Getenv("AWS_ACCESS_KEY_ID"),
 		secretKey: os.Getenv("AWS_SECRET_ACCESS_KEY"),
 	}, nil
+}
+
+func currentMonthCsv(objetos []string) []string {
+	currentMonth := strings.ToLower(time.Now().Month().String())
+
+	currentMonthPortuguese, ok := mesesMap[currentMonth]
+	if !ok {
+		fmt.Println("Nome do mês não encontrado:", currentMonth)
+		return nil
+	}
+
+	var csvMonth []string
+	for _, objeto := range objetos {
+		dir := strings.Split(objeto, "/")[0]
+
+		if strings.HasSuffix(strings.ToLower(dir), currentMonthPortuguese) {
+			if strings.HasSuffix(strings.ToLower(objeto), ".csv") {
+				csvMonth = append(csvMonth, objeto)
+			}
+		}
+	}
+
+	return csvMonth
 }
