9cb2ffa7c7a3318d16229fe270771b53c4db9d6e
patrik-rangel <patrik.souza@sptech.school>
2024-04-30 09:34:15 -0300

    update entity CostCenterInfo

--- smart-pay-service/internal/domain/entity/cost_center_info.go
+++ smart-pay-service/internal/domain/entity/cost_center_info.go
@@ -1,13 +1,13 @@
 package entity
 
-import "time"
+const filePatternCCI = `^centro-de-custos.*\.csv$`
 
 type CostCenterInfo struct {
-	AreaName       string
-	ExecutiveName  string
-	Email          string
-	TypeCostCenter string
-	Value          float64
-	StartDate      time.Time
-	EndDate        time.Time
+	AreaName       string     `csv:"nome_Area"`
+	ExecutiveName  string     `csv:"nome_Executivo"`
+	Email          string     `csv:"email"`
+	TypeCostCenter string     `csv:"tipo_Cc"`
+	Value          int        `csv:"orcamento_tri"`
+	StartDate      DateFormat `csv:"dat_inicio_orcamento"`
+	EndDate        DateFormat `csv:"dat_fim_orcamento"`
 }
