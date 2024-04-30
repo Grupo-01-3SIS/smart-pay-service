9d9b0fed62a29bd1c7bba83fb40d572c45daa10b
patrik-rangel <patrik.souza@sptech.school>
2024-04-30 09:34:41 -0300

    update entities MonthlyCosts and EmployeeMonthCosts

--- smart-pay-service/internal/domain/entity/monthly_costs.go
+++ smart-pay-service/internal/domain/entity/monthly_costs.go
@@ -2,21 +2,42 @@ package entity
 
 import "time"
 
+const FilePatternMC = `^custos-mensais.*\.csv$`
+const FilePatternEMC = `custos-funcionarios.*\.csv$`
+
+// DateFormat é um novo tipo que representa o formato de data "yyyy/MM/dd".
+type DateFormat string
+
+// UnmarshalCSV implementa a interface csv.Unmarshaler.
+func (d *DateFormat) UnmarshalCSV(s string) error {
+	t, err := time.Parse("2006/01/02", s)
+	if err != nil {
+		return err
+	}
+	*d = DateFormat(t.Format("2006/01/02"))
+	return nil
+}
+
+// MarshalCSV implementa a interface csv.Marshaler.
+func (d DateFormat) MarshalCSV() (string, error) {
+	return string(d), nil
+}
+
 type MonthlyCosts struct {
-	AreaName        string
-	Date            time.Time
-	Responsible     string
-	Description     string
-	Value           float64
-	ExpenseCategory string
-	PaymentMethod   string
-	Observation     string
+	AreaName        string     `csv:"id_Cc"`
+	Date            DateFormat `csv:"data"`
+	Responsible     string     `csv:"responsavel_custo"`
+	Description     string     `csv:"desc_transacao"`
+	Value           float64    `csv:"valor"`
+	ExpenseCategory string     `csv:"categoria_despesa"`
+	PaymentMethod   string     `csv:"metodo_pagto"`
+	Observation     string     `csv:"obs"`
 }
 
 type EmployeeMonthlyCosts struct {
-	AreaName     string
-	EmployeeName string
-	JobTitle     string
-	Position     string
-	Salary       string
+	AreaName     string `csv:"id_Cc"`
+	EmployeeName string `csv:"nome"`
+	JobTitle     string `csv:"cargo"`
+	Position     string `csv:"senioridade"`
+	Salary       string `csv:"salario"`
 }
