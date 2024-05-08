7941c330e021df1b6a59962f09010591f62f3eee
patrik-rangel <patrik.souza@sptech.school>
2024-05-08 18:22:40 -0300

    update main

--- smart-pay-service/cmd/main.go
+++ smart-pay-service/cmd/main.go
@@ -4,8 +4,8 @@ import (
 	"context"
 	"fmt"
 	"os"
-	"smart-pay-service/internal/domain/entity"
 	"smart-pay-service/internal/domain/service"
+	"smart-pay-service/internal/resource/database"
 	s3_client "smart-pay-service/internal/s3"
 	"strings"
 
@@ -35,64 +35,12 @@ func main() {
 		fileName := strings.Split(obj, "/")
 		err := s3Client.DownloadCsv("smartpay", obj, fileName[len(fileName)-1])
 		if err != nil {
+			println(err)
 			panic("falha no download")
 		}
 	}
 
-	service := service.NewCostCenterService(nil)
-
-	filePatterns := []string{`^custos-mensais.*\.csv$`, `^custos-funcionarios.*\.csv$`, `^centro-de-custos.*\.csv$`}
-
-	// for para dar o marshal nos 3 csv
-	for _, pattern := range filePatterns {
-		_, err := service.UnmarshalCSVData(context.Background(), pattern)
-		if err != nil {
-			panic("falha no marshal")
-		}
-	}
-}
-
-// Metodo para printar o resultado do Marshal do csv custos-mensais...csv
-func PrintMonthlyCosts(seq []*entity.MonthlyCosts) {
-	// Iterar sobre cada MonthlyCosts
-	for _, monthlyCost := range seq {
-		// Imprimir o valor de cada campo da struct MonthlyCosts
-		fmt.Println("AreaName:", monthlyCost.AreaName)
-		fmt.Println("Date:", monthlyCost.Date)
-		fmt.Println("Responsible:", monthlyCost.Responsible)
-		fmt.Println("Description:", monthlyCost.Description)
-		fmt.Println("Value:", monthlyCost.Value)
-		fmt.Println("ExpenseCategory:", monthlyCost.ExpenseCategory)
-		fmt.Println("PaymentMethod:", monthlyCost.PaymentMethod)
-		fmt.Println("Observation:", monthlyCost.Observation)
-		fmt.Println()
-	}
-}
-
-// Metodo para printar o resultado do Marshal do csv custos-funcionarios...csv
-func PrintEmployeeMonthlyCosts(seq []*entity.EmployeeMonthlyCosts) {
-	for _, employeeMonthlyCost := range seq {
-		fmt.Println("AreaName:", employeeMonthlyCost.AreaName)
-		fmt.Println("EmployeeName:", employeeMonthlyCost.EmployeeName)
-		fmt.Println("JobTitle:", employeeMonthlyCost.JobTitle)
-		fmt.Println("Position:", employeeMonthlyCost.Position)
-		fmt.Println("Salary:", employeeMonthlyCost.Salary)
-		fmt.Println()
-	}
-}
-
-// Metodo para printar o resultado do Marshal do csv centro-de-custos...csv
-
-func PrintCostCenter(seq []*entity.CostCenterInfo) {
-	// Iterar sobre cada MonthlyCosts
-	for _, costCenter := range seq {
-		fmt.Println("AreaName:", costCenter.AreaName)
-		fmt.Println("ExecutiveName:", costCenter.ExecutiveName)
-		fmt.Println("Email:", costCenter.Email)
-		fmt.Println("TypeCostCenter:", costCenter.TypeCostCenter)
-		fmt.Println("Value:", costCenter.Value)
-		fmt.Println("StartDate:", costCenter.StartDate)
-		fmt.Println("EndDate:", costCenter.EndDate)
-		fmt.Println()
-	}
+	gtw := database.NewDatabase()
+	service := service.NewCostCenterService(gtw)
+	service.RunService(context.Background())
 }
