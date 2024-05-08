fb35c1fa83ed5cfb5ef696d56911741553116543
patrik-rangel <patrik.souza@sptech.school>
2024-05-08 18:18:11 -0300

    update script create table

--- smart-pay-service/tools/postgres/scripts/1-tables.sql
+++ smart-pay-service/tools/postgres/scripts/1-tables.sql
@@ -1,82 +1,82 @@
+-- Table Orcamento_trimestral
+CREATE TABLE IF NOT EXISTS smartpay.orcamento_trimestral (
+  idorcamento_trimestral SERIAL PRIMARY KEY,
+  data_inicio TIMESTAMP NOT NULL,
+  data_fim TIMESTAMP NOT NULL,
+  orcamento_trimestral DOUBLE PRECISION NOT NULL
+);
+
+-- Table Centro_de_Custos
+CREATE TABLE IF NOT EXISTS smartpay.centro_de_custos (
+  idcentro_de_custos SERIAL PRIMARY KEY,
+  nome_centro VARCHAR(45) NOT NULL,
+  tipo VARCHAR(45) NOT NULL,
+  orcamento_trimestral INT NOT NULL,
+  fk_orcamento_trimestral INT NOT NULL,
+  CONSTRAINT fk_centro_de_custos_orcamento_trimestral1
+    FOREIGN KEY (fk_orcamento_trimestral)
+    REFERENCES smartpay.orcamento_trimestral (idorcamento_trimestral)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+);
+
 -- Table Executivo
 CREATE TABLE IF NOT EXISTS smartpay.executivo (
   id_executivo SERIAL PRIMARY KEY,
   nome_executivo VARCHAR(45) NOT NULL,
   email VARCHAR(45) NOT NULL,
-  area VARCHAR(45) NOT NULL
-);
-
--- Table Area
-CREATE TABLE IF NOT EXISTS smartpay.area (
-  id_area SERIAL PRIMARY KEY,
-  nome_area VARCHAR(45) NOT NULL
+  area VARCHAR(45) NOT NULL,
+  fk_centro_de_custos INT NOT NULL,
+  CONSTRAINT fk_executivo_centro_de_custos1
+    FOREIGN KEY (fk_centro_de_custos)
+    REFERENCES smartpay.centro_de_custos (idcentro_de_custos)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
 );
 
 -- Table Funcionario
 CREATE TABLE IF NOT EXISTS smartpay.funcionario (
-  id_funcionario SERIAL PRIMARY KEY,
-  nome_funcionario VARCHAR(45) NOT NULL,
+  idfuncionarios SERIAL PRIMARY KEY,
+  nome_funcionarios VARCHAR(45) NOT NULL,
   cargo VARCHAR(45) NOT NULL,
   senioridade VARCHAR(45) NOT NULL,
-  salario FLOAT NOT NULL,
-  vt FLOAT NOT NULL,
-  vr FLOAT NOT NULL
+  salario DOUBLE PRECISION NOT NULL,
+  fk_centro_de_custos INT NOT NULL,
+  CONSTRAINT fk_funcionario_centro_de_custos1
+    FOREIGN KEY (fk_centro_de_custos)
+    REFERENCES smartpay.centro_de_custos (idcentro_de_custos)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
 );
 
--- Table Orcamento_trimestral
-CREATE TABLE IF NOT EXISTS smartpay.orcamento_trimestral (
-  id_orcamento_trimestral SERIAL PRIMARY KEY,
-  data_inicio TIMESTAMP NOT NULL,
-  data_fim TIMESTAMP NOT NULL,
-  orcamento_trimestral FLOAT NOT NULL
-);
-
--- Table Centro_de_Custos
-CREATE TABLE IF NOT EXISTS smartpay.centro_de_custos (
-  id_centro_de_custos SERIAL PRIMARY KEY,
-  nome_centro VARCHAR(45) NOT NULL,
-  tipo VARCHAR(45) NOT NULL,
-  orcamento_trimestral INT NOT NULL,
-  fk_executivo INT NOT NULL,
-  fk_area INT NOT NULL,
-  fk_funcionario INT NOT NULL,
-  fk_orcamento_trimestral INT NOT NULL,
-  CONSTRAINT fk_centro_de_custos_executivo
-    FOREIGN KEY (fk_executivo)
-    REFERENCES smartpay.executivo (id_executivo)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT fk_centro_de_custos_area
-    FOREIGN KEY (fk_area)
-    REFERENCES smartpay.area (id_area)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT fk_centro_de_custos_funcionario
-    FOREIGN KEY (fk_funcionario)
-    REFERENCES smartpay.funcionario (id_funcionario)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT fk_centro_de_custos_orcamento_trimestral
-    FOREIGN KEY (fk_orcamento_trimestral)
-    REFERENCES smartpay.orcamento_trimestral (id_orcamento_trimestral)
+-- Table Area
+CREATE TABLE IF NOT EXISTS smartpay.area (
+  idarea SERIAL PRIMARY KEY,
+  nome_area VARCHAR(45) NOT NULL,
+  fk_centro_de_custos INT NOT NULL,
+  CONSTRAINT fk_area_centro_de_custos1
+    FOREIGN KEY (fk_centro_de_custos)
+    REFERENCES smartpay.centro_de_custos (idcentro_de_custos)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION
 );
 
 -- Table Gastos_variaveis
 CREATE TABLE IF NOT EXISTS smartpay.gastos_variaveis (
-  id_gastos_variaveis SERIAL PRIMARY KEY,
-  tipo_variavel VARCHAR(45) NOT NULL,
-  valor FLOAT NOT NULL,
+  idgastos_variaveis SERIAL PRIMARY KEY,
+  -- tipo_variavel VARCHAR(255),
+  valor DOUBLE PRECISION NOT NULL,
   categoria_despesa VARCHAR(45) NOT NULL,
-  desc_transacao VARCHAR(45),
+  desc_transacao VARCHAR(255),
   metodo_pagto VARCHAR(45),
-  obs VARCHAR(45),
-  data TIMESTAMP NOT NULL,
-  fk_funcionario INT NOT NULL,
-  CONSTRAINT fk_gastos_variaveis_funcionario
-    FOREIGN KEY (fk_funcionario)
-    REFERENCES smartpay.funcionario (id_funcionario)
+  obs VARCHAR(255),
+  data DATE NOT NULL,
+  responsavel VARCHAR(100) NOT NULL,
+  fk_centro_de_custos INT NOT NULL,
+  CONSTRAINT fk_gastos_variaveis_centro_de_custos1
+    FOREIGN KEY (fk_centro_de_custos)
+    REFERENCES smartpay.centro_de_custos (idcentro_de_custos)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION
 );
+
